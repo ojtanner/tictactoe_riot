d3e9fdcdd96713ed184d3ae44fc30c529a85a976
ojtanner <oliver-tanner@outlook.com>
2019-02-17 18:20:00 +0100

    Add rudimentary logic to display player mark

--- public/js/src/index.js
+++ public/js/src/index.js
@@ -1,3 +1,23 @@
 "use strict";
 
 riot.mount("*");
+
+let currentPlayer = "X";
+
+function getCurrentPlayer() {
+    return currentPlayer;
+}
+
+function setCurrentPlayer(player) {
+    currentPlayer = player;
+}
+
+function changePlayer() {
+    console.log("Before change", getCurrentPlayer());
+    if(getCurrentPlayer() === "X") {
+        setCurrentPlayer("O");
+    } else {
+        setCurrentPlayer("X");
+    }
+    console.log("After change", getCurrentPlayer())
+}
