508e80b4c9e386879f9900a293bff0eba94aaf6e
ojtanner <oliver-tanner@outlook.com>
2019-02-24 16:21:51 +0100

    Change player variables to pseudo-enums. Add semicolons.

--- public/js/src/index.js
+++ public/js/src/index.js
@@ -2,22 +2,26 @@
 
 riot.mount("*");
 
-let currentPlayer = "X";
+const players = {
+    x: "X",
+    o: "O"
+};
+let currentPlayer = players.x;
 
 function getCurrentPlayer() {
     return currentPlayer;
-}
+};
 
 function setCurrentPlayer(player) {
     currentPlayer = player;
-}
+};
 
 function changePlayer() {
     console.log("Before change", getCurrentPlayer());
-    if(getCurrentPlayer() === "X") {
-        setCurrentPlayer("O");
+    if(getCurrentPlayer() === players.x) {
+        setCurrentPlayer(players.o);
     } else {
-        setCurrentPlayer("X");
+        setCurrentPlayer(players.x);
     }
     console.log("After change", getCurrentPlayer())
-}
+};
