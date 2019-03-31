078f94fc8128d6b0f41a162866ed13dec908bd4c
ojtanner <oliver-tanner@outlook.com>
2019-03-31 21:13:58 +0200

    Move index html

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ const app = express();
 const path = require('path');
 const port = (process.env.PORT || 8000);
 
-app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.static(path.join(__dirname, '/public/dist')));
 
 app.get('/', (request, response) => {
     response.header({'Content-Type': 'text/html'});
-    response.sendFile(path.join(__dirname, '/public/html/index.html'));
+    response.sendFile(path.join(__dirname, '/public/dist/index.html'));
 });
 
 app.listen(port, () => {
