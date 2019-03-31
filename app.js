82e5ab37f4cb039e244d9b5de1f80ae750e120c2
ojtanner <oliver-tanner@outlook.com>
2019-03-31 21:21:40 +0200

    Change __dirname to process.cwd in app

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ const app = express();
 const path = require('path');
 const port = (process.env.PORT || 8000);
 
-app.use(express.static(path.join(__dirname, '/public/dist')));
+app.use(express.static(path.join(process.cwd() + '/public/dist')));
 
 app.get('/', (request, response) => {
     response.header({'Content-Type': 'text/html'});
-    response.sendFile(path.join(__dirname, '/public/dist/index.html'));
+    response.sendFile(path.join(process.cwd() + '/public/dist/index.html'));
 });
 
 app.listen(port, () => {
